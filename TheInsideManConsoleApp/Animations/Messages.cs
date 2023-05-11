f2067da7d6ad96c8f7e2c557695b12161b3ddc14
Joaoms98 <jvm1800@gmail.com>
2023-05-11 12:14:11 -0300

    feat: add stage 2

--- TheInsideManConsoleApp/Animations/Messages.cs
+++ TheInsideManConsoleApp/Animations/Messages.cs
@@ -13,20 +13,21 @@ namespace TheInsideManConsoleApp.Animations;
       \/_/  \/_/\/_/\/____/     \/_____/\/_/\/_/\/___/  \/_/\/__,_ /\/____/     \/_/ \/_/\/__/\/_/\/_/\/_/
                                                                                                           
                                                                                                           
-                                         Press Enter To Start                                                                      
+                                         Press Enter To Start
+                            Ajuste a tela até que todo o logo esteja visível                                                                      
         ";
 
        private string instructions =  
         @"
-                                                 Instructions:                      
-                                        _______     _______     _______ 
-                                       |\     /|   |\     /|   |\     /|
-                                       | +---+ |   | +---+ |   | +---+ |
-                                       | |   | |   | |   | |   | |   | |
-                                       | |<- | |   | |any| |   | |-> | |
-                                       | +---+ |   | +---+ |   | +---+ |
-                                       |/_____\|   |/_____\|   |/_____\|
-                                        MoveLeft    NotMove    MoveRight                                 
+                                              Instructions:                      
+                                     _______     _______     _______ 
+                                    |\     /|   |\     /|   |\     /|
+                                    | +---+ |   | +---+ |   | +---+ |
+                                    | |   | |   | |   | |   | |   | |
+                                    | |<- | |   | | ↵ | |   | | ->| |
+                                    | +---+ |   | +---+ |   | +---+ |
+                                    |/_____\|   |/_____\|   |/_____\|
+                                    Esquerda    Selecionar   Direita                                 
         ";
 
        private string gameOver =  
@@ -43,12 +44,33 @@ namespace TheInsideManConsoleApp.Animations;
                                                                            ░                    
         ";
 
+       private string text1 =  
+        @"
+●▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬●
+     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis 
+    condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi tristique senectus et netus et 
+    malesuada fames ac turpis egestas. Nulla at risus. Quisque purus magna, auctor et, sagittis ac, posuere
+    eu, lectus. Nam mattis, felis ut adipiscing. 
+●▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬●
+        ";
+
+       private string text2 =  
+        @"
+●▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬●
+     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis 
+    condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi tristique senectus et netus et 
+    malesuada fames ac turpis egestas. Nulla at risus. Quisque purus magna, auctor et, sagittis ac, posuere
+    eu, lectus. Nam mattis, felis ut adipiscing. 
+●▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬●
+        ";
+
         public void LogoAnimation()
         {
             do
             {
                Console.WriteLine(logo);
             }while(Console.ReadKey().Key != ConsoleKey.Enter);
+            Console.Clear();
         }
 
         public void InstructionsAnimation()
@@ -63,5 +85,20 @@ namespace TheInsideManConsoleApp.Animations;
         public void GameOverAnimation()
         {
             Console.WriteLine(gameOver);
+            Environment.Exit(1);
+        }
+
+        public void Text1Animation()
+        {
+            Console.WriteLine(text1);
+        }
+        public void Text2Animation()
+        {
+            do
+            {
+                Console.Clear();
+                Console.WriteLine(text2);
+            }while(Console.ReadKey().Key != ConsoleKey.Enter);
+            Console.Clear();
         }
     }
